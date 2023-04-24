919850d560363dc5a262244a383f4cd281993cd4
VerneriLop <verneri.lopperi@gmail.com>
2023-04-24 15:10:01 +0300

    uibuild

--- Osa3/harjoitus/index.js
+++ Osa3/harjoitus/index.js
@@ -1,16 +1,35 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const app = express()
 //web-sovelluksen selaimessa suoritettava JavaScript-koodi saa oletusarvoisesti kommunikoida 
 //vain samassa originissa olevan palvelimen kanssa. Koska palvelin on localhostin portissa 3001 
 //ja frontend localhostin portissa 3000, niiden origin ei ole sama.
 //https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy
 //Voimme sallia muista origineista tulevat pyynnöt käyttämällä Noden cors-middlewarea
 const cors = require('cors')
-app.use(cors())
-app.use(express.static('build')) //osaa tarjoilla staattista html sivua.
 require('dotenv').config()
 const Note = require('./models/note')
+
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+app.use(express.json());
+app.use(cors())
+app.use(express.static('build')) //osaa tarjoilla staattista html sivua.
+app.use(requestLogger)
+
+
+
 
 /*let notes = [
     {
@@ -30,9 +49,11 @@ const Note = require('./models/note')
     }
 ]*/
 
+/*
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
+*/
 
 /*
 app.get('/api/notes', (req, res) => {
@@ -123,6 +144,74 @@ app.get('/api/notes', (request, response) => {
       response.json(notes)
     })
 })
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
+    }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+        
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+  
+app.use(unknownEndpoint)
+app.use(errorHandler) //tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+
 
 
 const PORT = process.env.PORT
