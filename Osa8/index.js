bcca6a96524ab12ec1d507ff8fc5cdbdd119dbba
VerneriLop <verneri.lopperi@gmail.com>
2023-04-28 13:38:13 +0300

    numeron päivitys

--- Osa8/index.js
+++ Osa8/index.js
@@ -57,6 +57,10 @@ const typeDefs = `
       street: String!
       city: String!
     ): Person
+    editNumber(
+        name: String!
+        phone: String!
+      ): Person
   }
 `
 
@@ -97,6 +101,19 @@ const resolvers = {
         return person
     }
   },
+  Mutation: {
+    // ...
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if (!person) {
+        return null
+      }
+  
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }   
+  }
 }
 
 const server = new ApolloServer({
