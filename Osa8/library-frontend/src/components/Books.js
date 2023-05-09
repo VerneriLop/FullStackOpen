67c9e111514de105986468675868a219a0735ee3
VerneriLop <verneri.lopperi@gmail.com>
2023-05-09 11:09:18 +0300

    8.9. done

--- Osa8/library-frontend/src/components/Books.js
+++ Osa8/library-frontend/src/components/Books.js
@@ -1,9 +1,17 @@
+import { ALL_BOOKS } from "../queries";
+import { useQuery } from '@apollo/client'
+
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS);
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -16,7 +24,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {result.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
